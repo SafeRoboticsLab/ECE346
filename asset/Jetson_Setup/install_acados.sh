590e9a51e8e6a68604a2d658bebbdc8d222c9578
Zixu Zhang <zixuz@princeton.edu>
2022-01-24 22:58:48 -0500

    update readme for lab0

--- asset/Jetson_Setup/install_acados.sh
+++ asset/Jetson_Setup/install_acados.sh
@@ -43,13 +43,15 @@ source ACADOS_env/bin/activate
 
 # set up env in virtualenv
 pip install -e acados/interfaces/acados_template
-pip install pyyaml rospkg empy sympy spatialmath-python pyclothoids
+pip install pyyaml rospkg empy sympy 
 
-# install gfortran
-sudo apt-get install gfortran
-# install pyspline
-git clone https://github.com/mdolab/pyspline.git
-cd pyspline
-cp config/defaults/config.LINUX_GFORTRAN.mk config/config.mk
-make
-pip install .
+# # install dependence for MPCC
+# pip install spatialmath-python pyclothoids
+# # install gfortran
+# sudo apt-get install gfortran
+# # install pyspline
+# git clone https://github.com/mdolab/pyspline.git
+# cd pyspline
+# cp config/defaults/config.LINUX_GFORTRAN.mk config/config.mk
+# make
+# pip install .
