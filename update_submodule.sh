6168b0ea92ac21a6c128ea5723627caed78d0955
Zixu Zhang <zixuz@princeton.edu>
2023-04-24 11:14:52 -0400

    update submodule:

---
+++ update_submodule.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CUR_DIR=$(pwd)
+
+git submodule update --init --recursive
+
+# SLAM message
+cd $CUR_DIR/ROS_Core/src/Utility/AprilTagSLAM_ROS
+git checkout msg_only
+git pull
+
+# Interface
+cd $CUR_DIR/ROS_Core/src/Utility/Interface
+git checkout main
+git pull
+
+# Planning
+cd $CUR_DIR/ROS_Core/src/Utility/Labs/Lab1
+git checkout main
+git pull
+
+
+# Message
+cd $CUR_DIR/ROS_Core/src/Utility/ROS_msgs
+git checkout main
+git pull
+
+# Routing
+cd $CUR_DIR/ROS_Core/src/Utility/Routing
+git checkout pylanelet
+git pull
+
+cd $CUR_DIR
